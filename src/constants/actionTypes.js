cecb1af8bc7502027a064dd2a19ad0c65e384ec6
Samsritha <samrith@gmail.com>
2024-02-04 04:10:42 +0530

    final push 2.0

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,3 @@
-export const URL = "http://10.200.230.209:8000";
+export const URL = "http://localhost:8000";
 export const ADD_USER_INFO = "ADD_USER_INFO";
 export const GET_USER_INFO = "GET_USER_INFO";
